8d38809ed59500ab43dc50b33e7f2387c67bf31d
mansi-2007 <porvadmansi@gmail.com>
2024-08-06 12:29:22 +0530

    Add files via upload

---
+++ exam/qu1.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<conio.h>
+
+int  main()
+{
+	int a,b,c,d;
+	
+	printf("enter you a");
+	scanf("%d",&a);
+	printf("enter you b");
+	scanf("%d",&b);
+	printf("enter you c");
+	scanf("%d",&c);
+	printf("enter you d");
+	scanf("%d",&d);
+	
+	// a,b,c,d
+	
+	if(a>b && a>c && a>d)
+	{
+		if(a>b)
+		{
+			if(a>c)
+			
+				
+			
+	}
+		if(a>d)
+		
+		}
+		else
+		{
+			printf("a is max");
+		}
+		if(b>a && b>c && b>d)
+		{
+			if(b>a)
+			{
+				if(b>c)
+				{
+					if(b>d)
+				}
+				else
+				{
+					pirtf("b is max");
+				}
+				if(c>a && c>b && c>d)
+				{
+					if(c>a)
+					
+				}
+					if(c>b)
+					{
+						if(c>d)
+						{
+							if(c>d)
+						}
+						else
+						{
+							printf("c is max");
+						}
+						if(d>a && d>b && b>c)
+						{
+							if(d>a)
+							{
+								if(d>b)
+								{
+									if(d>c)
+							}
+							else
+							{
+								printf("d is max");
+								}	
+								
+								}
+							}					
+				
+				
+}
+		}			
+			
+		
+		}
+			
+		
+		
+			
+		
+	
+
+	
+
+	
+
+	
+	
+
+
+	
+	
+
+	
+	
+	
