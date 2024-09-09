cbf15d53486b21f4ce3f969ffef43c88c99fd873
mansi-2007 <porvadmansi@gmail.com>
2024-09-09 11:51:07 +0530

    Add files via upload

---
+++ udf/udf qu2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void divisible(int n)
+{
+	if(n%3==0&&n%5==0)
+	{
+		printf("value is divisible by both 3 & 5  ");
+	}
+	else if(n%3==0)
+	{
+		printf("value is divisible by  3   ");
+	}
+	else if(n%5==0)
+	{
+		printf("value is divisible by  5  ");
+	}
+	else 
+	{
+		printf("value is not divisible by both 3 &5 ");
+	}
+	
+	void main(divisible)
+	{
+		divisible(15);
+	}
+
+}
